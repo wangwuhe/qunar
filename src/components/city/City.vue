d284b6cdeaffbdbe572966b563c8a92c363cc480
he15838949284 <2356142622@qq.com>
2020-08-17 12:12:52 +0800

    add scroll

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -1,22 +1,16 @@
 <template>
   <div class="city">
     <city-header></city-header>
-    <city-hot :hotCities="hotCities"></city-hot>
-    <city-sort :cities="cities"></city-sort>
-    <city-list :cities="cities"></city-list>
+    <city-list :hotCities="hotCities" :cities="cities"></city-list>
   </div>
 </template>
 
 <script>
 import CityHeader from "./page/header";
-import CityHot from "./page/Hot";
-import CitySort from "./page/Sort";
 import CityList from "./page/List";
 export default {
   components: {
     CityHeader,
-    CityHot,
-    CitySort,
     CityList,
   },
   data() {
@@ -28,7 +22,6 @@ export default {
   mounted() {
     this.$http.get("/api/city.json").then((res) => {
       const data = res.data.data;
-      console.log(data);
       this.hotCities = data.hotCities;
       this.cities = data.cities;
     });
--- src/components/city/page/List.vue
+++ src/components/city/page/List.vue
@@ -1,27 +1,120 @@
 <template>
-  <div class="list">
-    <div v-for="(val,key) in cities" :key="key">
-      <div class="list-title">{{key}}</div>
-      <ul class="list-msg">
-        <li class="list-item" v-for="item in val" :key="item.id">{{item.name}}</li>
+<div ref="container" class="container">
+  <div>
+    <div class="hot">
+      <div class="hot-title">热门城市</div>
+      <ul class="hot-list">
+        <li class="hot-item" v-for="item in hotCities" :key="item.id">{{item.name}}</li>
       </ul>
     </div>
+
+    <div class="sort">
+      <div class="sort-title">字母排序</div>
+      <ul class="sort-list">
+        <li class="sort-item" v-for="(val,key) in cities" :key="key" @click="changeSort(key)">{{key}}</li>
+      </ul>
+    </div>
+
+    <div class="list">
+      <div v-for="(val,key) in cities" :key="key" :ref="key">
+        <div class="list-title">{{key}}</div>
+        <ul class="list-msg">
+          <li class="list-item" v-for="item in val" :key="item.id">{{item.name}}</li>
+        </ul>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
+import BScroll from 'better-scroll';
 export default {
-  props: ["cities"],
+  props: ["cities","hotCities"],
+  data() {
+    return {
+      scroll:''
+    }
+  },
+  mounted() {
+    let container=this.$refs['container']
+    this.scroll = new BScroll(container)
+  },
+  methods: {
+    changeSort(key){
+      console.log(this.$refs[key][0])
+    }
+  },
 };
 </script>
 
 <style scoped lang="stylus">
 @import '~css/common.styl';
 
-.list-title {
-  font-size: 12px;
+.container{
+  position: absolute;
+  overflow: hidden;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 12.222vw;
+  background: #f5f5f5;
+}
+
+.hot-title {
+  font-size: 3.333vw;
   color: #212121;
-  padding: 10px 15px;
+  padding: 2.778vw 4.167vw;
+}
+.hot-list {
+  position: relative;
+  overflow: hidden;
+  background: white;
+  font-size: 3.889vw;
+}
+.hot-list::before {
+  content: " ";
+  position: absolute;
+  height: 100%;
+  width: 33.333%;
+  left: 33.333%;
+  border-left: 0.278vw solid #ddd;
+  border-right: 0.278vw solid #ddd;
+}
+.hot-item {
+  color: #212121;
+  width: 33.33%;
+  text-align: center;
+  height: 12.5vw;
+  line-height: 12.5vw;
+  float: left;
+  border-bottom: 0.278vw solid #ddd;
+}
+
+.sort-title {
+  font-size: 3.333vw;
+  color: #212121;
+  padding: 2.778vw 4.167vw;
+}
+.sort-list {
+  position: relative;
+  overflow: hidden;
+  background: white;
+  font-size: 3.889vw;
+}
+.sort-item {
+  color: #212121;
+  width: 16.666%;
+  text-align: center;
+  height: 12.5vw;
+  line-height: 12.5vw;
+  float: left;
+}
+
+.list-title {
+  font-size: 3.333vw;
+  color: #212121;
+  padding: 2.778vw 4.167vw;
 }
 
 .list-msg {
@@ -36,8 +129,8 @@ export default {
   left: 25%;
   width: 25%;
   height: 100%;
-  border-left: 1px solid #ddd;
-  border-right: 1px solid #ddd;
+  border-left: 0.278vw solid #ddd;
+  border-right: 0.278vw solid #ddd;
 }
 
 .list-msg:after {
@@ -46,17 +139,17 @@ export default {
   left: 75%;
   width: 0%;
   height: 100%;
-  border-left: 1px solid #ddd;
+  border-left: 0.278vw solid #ddd;
 }
 
 .list-item {
   position: relative;
   width: 25%;
-  font-size: 15px;
+  font-size: 4.167vw;
   text-align: center;
-  line-height: 45px;
+  line-height: 12.5vw;
   float: left;
-  border-bottom: 1px solid #ddd;
+  border-bottom: 0.278vw solid #ddd;
   textOverflow();
 }
 </style>
